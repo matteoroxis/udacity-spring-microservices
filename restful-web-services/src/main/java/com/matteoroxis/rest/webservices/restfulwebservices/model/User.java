cb56b3ebb28eb2f5a2f57ee7c9701de867f5b1d3
matteo <matteo@DESKTOP-2604T4F>
2018-02-04 22:32:43 +0100

    JPA

--- restful-web-services/src/main/java/com/matteoroxis/rest/webservices/restfulwebservices/model/User.java
+++ restful-web-services/src/main/java/com/matteoroxis/rest/webservices/restfulwebservices/model/User.java
@@ -3,12 +3,18 @@ package com.matteoroxis.rest.webservices.restfulwebservices.model;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
 @ApiModel(description="All details about the user. ")
+@Entity
 public class User {
 	
+        @Id
+        @GeneratedValue
 	private Long id;
 	@Size(min=2,message="Name should have at least 2 characters")
         @ApiModelProperty(notes="Name should have at least 2 characters")
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  matteoroxis
+ * Created: 4-feb-2018
+ */
+insert into user values(1,sysdate(),'Mario');
+insert into user values(2,sysdate(),'Marta');
+insert into user values(3,sysdate(),'Manrico');
+

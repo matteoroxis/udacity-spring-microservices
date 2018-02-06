117bf0dff9e867575c75225a0a84c66ab89209e8
Matteo Rossi <32036260+matteoroxis@users.noreply.github.com>
2018-02-06 12:43:57 +0100

    JPA Repository

--- restful-web-services/src/main/java/com/matteoroxis/rest/webservices/restfulwebservices/model/User.java
+++ restful-web-services/src/main/java/com/matteoroxis/rest/webservices/restfulwebservices/model/User.java
@@ -3,61 +3,79 @@ package com.matteoroxis.rest.webservices.restfulwebservices.model;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
-@ApiModel(description="All details about the user. ")
+@ApiModel(description = "All details about the user. ")
 @Entity
 public class User {
-	
-        @Id
-        @GeneratedValue
+
+	@Id
+	@GeneratedValue
 	private Long id;
-	@Size(min=2,message="Name should have at least 2 characters")
-        @ApiModelProperty(notes="Name should have at least 2 characters")
-        private String name;
-        
-        @Past
-        @ApiModelProperty(notes="Birth date should be in the past")
+	@Size(min = 2, message = "Name should have at least 2 characters")
+	@ApiModelProperty(notes = "Name should have at least 2 characters")
+	private String name;
+
+	@Past
+	@ApiModelProperty(notes = "Birth date should be in the past")
 	private LocalDate birthday;
 	
-	public User(){
-		
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
+
+	public List<Post> getPosts() {
+		return posts;
 	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+	public User() {
+
+	}
+
 	public User(Long id, String name, LocalDate birthday) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.birthday = birthday;
 	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthday=" + birthday + "]";
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public LocalDate getBirthday() {
 		return birthday;
 	}
+
 	public void setBirthday(LocalDate birthday) {
 		this.birthday = birthday;
 	}
-	
-	
-	
-	
 
 }
--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -7,7 +7,11 @@
  * Author:  matteoroxis
  * Created: 4-feb-2018
  */
-insert into user values(1,sysdate(),'Mario');
-insert into user values(2,sysdate(),'Marta');
-insert into user values(3,sysdate(),'Manrico');
+insert into user values(10001,sysdate(),'Mario');
+insert into user values(10002,sysdate(),'Marta');
+insert into user values(10003,sysdate(),'Manrico');
+
+insert into post values(11001,'My First Post',10001);
+insert into post values(11002,'My Second Post',10002);
+insert into post values(11003,'My Third Post',10003);
 
